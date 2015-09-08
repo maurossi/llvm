cfb11bc43d4b829fd9c2ac8599f7a771e8ce72a7
Mauro Rossi <issor.oruam@gmail.com>
2015-09-09 00:51:38 +0200

    Enable R600 support on Android-x86 (by pstglia)

--- lib/ExecutionEngine/Android.mk
+++ lib/ExecutionEngine/Android.mk
@@ -16,3 +16,24 @@ LOCAL_MODULE_TAGS := optional
 
 include $(LLVM_HOST_BUILD_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+include $(CLEAR_VARS)
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+
+LOCAL_SRC_FILES := \
+        ExecutionEngineBindings.cpp \
+        ExecutionEngine.cpp \
+        RTDyldMemoryManager.cpp \
+        TargetSelect.cpp
+
+LOCAL_MODULE:= libLLVMExecutionEngine
+
+LOCAL_MODULE_TAGS := optional
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif
+
