4889c392e4fa3ce92537dcf3a42bb4d67951e738
Mauro Rossi <issor.oruam@gmail.com>
2015-12-19 15:50:38 +0100

    android: enable support for AMDGPU targets
    Inspired by former psglia work on LLVM 3.5.x, now applied to 3.8.0
    
    Gallium radeonsi driver requires LLVM.
    Some required features (like compiling the AMDGPU Taget itself) are not enabled.
    
    These changes enable AMDGPU targets,
    fix a problem with "intrinsic" due to a typo in tablegen command option,
    add some target device modules that was missing.
    
    Mesa makefiles will require changes to support new libLLVMAMDGPU* modules

--- lib/ExecutionEngine/Android.mk
+++ lib/ExecutionEngine/Android.mk
@@ -17,3 +17,22 @@ LOCAL_MODULE_HOST_OS := darwin linux windows
 
 include $(LLVM_HOST_BUILD_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+include $(CLEAR_VARS)
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+
+LOCAL_SRC_FILES := \
+	ExecutionEngineBindings.cpp \
+	ExecutionEngine.cpp \
+	GDBRegistrationListener.cpp \
+	SectionMemoryManager.cpp \
+	TargetSelect.cpp
+
+LOCAL_MODULE:= libLLVMExecutionEngine
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif
