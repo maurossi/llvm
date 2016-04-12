bf275d09632a6b253197b0c5e4dfb0663d8738f4
Chih-Hung Hsieh <chh@google.com>
2016-04-12 15:27:11 -0700

    Suppress many less important clang-tidy warnings.
    This will suppress about 256 warnings of google-explicit-constructor
    and 1200 warnings of google-build-using-namespace.
    
    BUG: 28150914
    BUG: 28150377
    Change-Id: Ie2783b586f2591c1677b6ac52d3527d87ee251c7

--- lib/Fuzzer/Android.mk
+++ lib/Fuzzer/Android.mk
@@ -28,6 +28,10 @@ libfuzzer_srcs := \
     FuzzerSHA1.cpp \
     FuzzerUtil.cpp \
 
+# Suppress clang-tidy warnings that should not apply to llvm.
+libfuzzer_tidy_checks := \
+    -google-build-using-namespace,-google-explicit-constructor
+
 include $(CLEAR_VARS)
 LOCAL_CLANG := true
 LOCAL_MODULE := libLLVMFuzzerNoMain
@@ -35,6 +39,7 @@ LOCAL_C_INCLUDES := external/compiler-rt/include
 LOCAL_SRC_FILES := $(libfuzzer_srcs)
 LOCAL_SANITIZE := never
 LOCAL_MULTILIB := both
+LOCAL_TIDY_CHECKS := $(libfuzzer_tidy_checks)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -44,6 +49,7 @@ LOCAL_SRC_FILES := FuzzerMain.cpp
 LOCAL_WHOLE_STATIC_LIBRARIES := libLLVMFuzzerNoMain
 LOCAL_SANITIZE := never
 LOCAL_MULTILIB := both
+LOCAL_TIDY_CHECKS := $(libfuzzer_tidy_checks)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -53,6 +59,7 @@ LOCAL_C_INCLUDES := external/compiler-rt/include
 LOCAL_SRC_FILES := $(libfuzzer_srcs)
 LOCAL_SANITIZE := never
 LOCAL_MULTILIB := both
+LOCAL_TIDY_CHECKS := $(libfuzzer_tidy_checks)
 include $(BUILD_HOST_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -62,4 +69,5 @@ LOCAL_SRC_FILES := FuzzerMain.cpp
 LOCAL_WHOLE_STATIC_LIBRARIES := libLLVMFuzzerNoMain
 LOCAL_SANITIZE := never
 LOCAL_MULTILIB := both
+LOCAL_TIDY_CHECKS := $(libfuzzer_tidy_checks)
 include $(BUILD_HOST_STATIC_LIBRARY)
