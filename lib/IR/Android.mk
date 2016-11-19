4fa3c06ec76f2be3f3f32366e342a4a2a758ed49
Mauro Rossi <issor.oruam@gmail.com>
2016-11-19 15:33:24 +0100

    android: lib/IR: fix building rules for llvm 3.8.1
    Fixes building rules for tblgen AttributesCompatFunc.inc
    and simplifies lib/IR .cpp sources list

--- lib/IR/Android.mk
+++ lib/IR/Android.mk
@@ -1,55 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 
-vmcore_SRC_FILES := \
-  AsmWriter.cpp \
-  Attributes.cpp \
-  AutoUpgrade.cpp \
-  BasicBlock.cpp \
-  Comdat.cpp \
-  ConstantFold.cpp \
-  ConstantRange.cpp \
-  Constants.cpp \
-  Core.cpp \
-  DataLayout.cpp \
-  DebugInfo.cpp \
-  DebugInfoMetadata.cpp \
-  DebugLoc.cpp \
-  DiagnosticInfo.cpp \
-  DiagnosticPrinter.cpp \
-  DIBuilder.cpp \
-  Dominators.cpp \
-  Function.cpp \
-  FunctionInfo.cpp \
-  GCOV.cpp \
-  GVMaterializer.cpp \
-  Globals.cpp \
-  IRBuilder.cpp \
-  IRPrintingPasses.cpp \
-  InlineAsm.cpp \
-  Instruction.cpp \
-  Instructions.cpp \
-  IntrinsicInst.cpp \
-  LLVMContext.cpp \
-  LLVMContextImpl.cpp \
-  LegacyPassManager.cpp \
-  Mangler.cpp \
-  MDBuilder.cpp \
-  Metadata.cpp \
-  MetadataTracking.cpp \
-  Module.cpp \
-  Operator.cpp \
-  Pass.cpp \
-  PassManager.cpp \
-  PassRegistry.cpp \
-  Statepoint.cpp \
-  Type.cpp \
-  TypeFinder.cpp \
-  Use.cpp \
-  User.cpp \
-  Value.cpp \
-  ValueSymbolTable.cpp \
-  ValueTypes.cpp \
-  Verifier.cpp
+vmcore_TBLGEN_TABLES := AttributesCompatFunc.inc
+vmcore_SRC_FILES := $(sort $(notdir $(wildcard $(LOCAL_PATH)/*.cpp)))
 
 # For the host
 # =====================================================
@@ -58,12 +10,14 @@ include $(CLEAR_VARS)
 REQUIRES_RTTI := 1
 
 LOCAL_SRC_FILES := $(vmcore_SRC_FILES)
+TBLGEN_TABLES := $(vmcore_TBLGEN_TABLES)
 
 LOCAL_MODULE:= libLLVMCore
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
 include $(LLVM_HOST_BUILD_MK)
+include $(LLVM_TBLGEN_RULES_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
@@ -76,12 +30,14 @@ ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
 REQUIRES_RTTI := 1
 
 LOCAL_SRC_FILES := $(vmcore_SRC_FILES)
+TBLGEN_TABLES := $(vmcore_TBLGEN_TABLES)
 
 LOCAL_MODULE:= libLLVMCore
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(call local-generated-sources-dir)
 
 include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_TBLGEN_RULES_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_STATIC_LIBRARY)
