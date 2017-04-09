maurossi/llvm

commit f223f03f0befbce01df7723d9ab64affcb3c8a1d

android: enable support for AMDGPU targets for radeonsi
Inspired by former Psglia work on LLVM 3.5.x and Mauro work on 3.7.0.
Now applied to LLVM 3.8.0 of Android 7.0.

Gallium radeonsi driver requires LLVM. Some required features
(like compiling the R600 Taget itself) are not enabled.

These changes enable AMDGPU targets and fix a problem with "intrinsic"
due to a typo in tablegen command option.

lib/Target/AMDGPU/AsmParser/Android.mk
@@ -1,0 +1,51 @@
+LOCAL_PATH := $(call my-dir)
+
+amdgpu_asmparser_SRC_FILES := \
+  AMDGPUAsmParser.cpp
+
+amdgpu_asmparser_TBLGEN_TABLES := \
+  AMDGPUGenAsmMatcher.inc
+
+amdgpu_asm_parser_TBLGEN_TD_DIR := $(LOCAL_PATH)/..
+
+amdgpu_asm_parser_C_INCLUDES :=	$(LOCAL_PATH)/..
+
+#===---------------------------------------------------------------===
+# libAMDGPUAsmParser (host)
+#===---------------------------------------------------------------===
+include $(CLEAR_VARS)
+include $(CLEAR_TBLGEN_VARS)
+
+LOCAL_MODULE := libLLVMAMDGPUAsmParser
+LOCAL_MODULE_HOST_OS := darwin linux windows
+LOCAL_SRC_FILES := $(amdgpu_asm_parser_SRC_FILES)
+LOCAL_C_INCLUDES := $(amdgpu_asm_parser_C_INCLUDES)
+TBLGEN_TABLES := $(amdgpu_asm_parser_TBLGEN_TABLES)
+TBLGEN_TD_DIR := $(amdgpu_asm_parser_TBLGEN_TD_DIR)
+
+include $(LLVM_HOST_BUILD_MK)
+include $(LLVM_TBLGEN_RULES_MK)
+include $(LLVM_GEN_ATTRIBUTES_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+
+#===---------------------------------------------------------------===
+# libAMDGPUAsmParser (target)
+#===---------------------------------------------------------------===
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+include $(CLEAR_VARS)
+include $(CLEAR_TBLGEN_VARS)
+
+LOCAL_MODULE := libLLVMAMDGPUAsmParser
+LOCAL_SRC_FILES := $(amdgpu_asm_parser_SRC_FILES)
+LOCAL_C_INCLUDES := $(amdgpu_asm_parser_C_INCLUDES)
+TBLGEN_TABLES := $(amdgpu_asm_parser_TBLGEN_TABLES)
+TBLGEN_TD_DIR := $(amdgpu_asm_parser_TBLGEN_TD_DIR)
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_TBLGEN_RULES_MK)
+include $(LLVM_GEN_ATTRIBUTES_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif

Android.mk
@@ -96,6 +96,17 @@ subdirs += \
   lib/Target/X86/TargetInfo \
   lib/Target/X86/Utils
 
+ifneq ($(filter radeonsi,$(BOARD_GPU_DRIVERS)),)
+# AMDGPU Code Generation Libraries
+subdirs += \
+  lib/Target/AMDGPU \
+  lib/Target/AMDGPU/AsmParser \
+  lib/Target/AMDGPU/InstPrinter \
+  lib/Target/AMDGPU/MCTargetDesc \
+  lib/Target/AMDGPU/TargetInfo \
+  lib/Target/AMDGPU/Utils
+endif
+
 # LLVM Command Line Tools
 subdirs += \
   tools/bugpoint \

lib/ExecutionEngine/Android.mk
@@ -1,21 +1,33 @@
 LOCAL_PATH:= $(call my-dir)
 
-# For the host
-# =====================================================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
+executionengine_SRC_FILES := \
 	ExecutionEngineBindings.cpp \
 	ExecutionEngine.cpp \
 	GDBRegistrationListener.cpp \
 	SectionMemoryManager.cpp \
 	TargetSelect.cpp
 
-LOCAL_MODULE:= libLLVMExecutionEngine
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
 
+LOCAL_MODULE := libLLVMExecutionEngine
 LOCAL_MODULE_HOST_OS := darwin linux windows
+LOCAL_SRC_FILES := $(executionengine_SRC_FILES)
 
 include $(LLVM_HOST_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
-include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libLLVMExecutionEngine
+LOCAL_SRC_FILES := $(executionengine_SRC_FILES)
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_ATTRIBUTES_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif

lib/ExecutionEngine/Interpreter/Android.mk
@@ -1,19 +1,34 @@
 LOCAL_PATH:= $(call my-dir)
 
-# For the host
-# =====================================================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES :=      \
+interpreter_SRC_FILES := \
   Execution.cpp         \
   ExternalFunctions.cpp \
   Interpreter.cpp       \
 
-LOCAL_MODULE:= libLLVMInterpreter
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(interpreter_SRC_FILES)
+LOCAL_MODULE := libLLVMInterpreter
+
+LOCAL_MODULE_HOST_OS := darwin linux windows
 
 include $(LLVM_HOST_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(interpreter_SRC_FILES)
+LOCAL_MODULE := libLLVMInterpreter
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_ATTRIBUTES_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif

lib/ExecutionEngine/MCJIT/Android.mk
@@ -1,17 +1,30 @@
 LOCAL_PATH:= $(call my-dir)
 
+mcjit_SRC_FILES := \
+  MCJIT.cpp
+
 # For the host
 # =====================================================
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-  MCJIT.cpp
-
-LOCAL_MODULE:= libLLVMMCJIT
+LOCAL_SRC_FILES := $(mcjit_SRC_FILES)
+LOCAL_MODULE := libLLVMMCJIT
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
 include $(LLVM_HOST_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
-include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(mcjit_SRC_FILES)
+LOCAL_MODULE := libLLVMMCJIT
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_ATTRIBUTES_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif

lib/ExecutionEngine/Orc/Android.mk
@@ -1,16 +1,17 @@
 LOCAL_PATH:= $(call my-dir)
 
-# For the host
-# =====================================================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
+orcjit_SRC_FILES := \
   ExecutionUtils.cpp \
   IndirectionUtils.cpp \
   OrcMCJITReplacement.cpp \
   OrcTargetSupport.cpp
 
-LOCAL_MODULE:= libLLVMOrcJIT
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(orcjit_SRC_FILES)
+LOCAL_MODULE := libLLVMOrcJIT
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
@@ -18,3 +19,17 @@ include $(LLVM_HOST_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(orcjit_SRC_FILES)
+LOCAL_MODULE := libLLVMOrcJIT
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_ATTRIBUTES_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif

lib/ExecutionEngine/RuntimeDyld/Android.mk
@@ -1,10 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
-# For the host
-# =====================================================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
+runtimedyld_SRC_FILES := \
   RTDyldMemoryManager.cpp \
   RuntimeDyldChecker.cpp \
   RuntimeDyld.cpp \
@@ -12,9 +8,26 @@ LOCAL_SRC_FILES := \
   RuntimeDyldELF.cpp \
   RuntimeDyldMachO.cpp
 
-LOCAL_MODULE:= libLLVMRuntimeDyld
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(runtimedyld_SRC_FILES)
+LOCAL_MODULE := libLLVMRuntimeDyld
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
 include $(LLVM_HOST_BUILD_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(runtimedyld_SRC_FILES)
+LOCAL_MODULE := libLLVMRuntimeDyld
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif

lib/IR/Android.mk
@@ -78,6 +78,8 @@ REQUIRES_RTTI := 1
 LOCAL_SRC_FILES := $(vmcore_SRC_FILES)
 
 LOCAL_MODULE:= libLLVMCore
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(call local-generated-sources-dir)
 
 include $(LLVM_DEVICE_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)

commit 475bcf2591a7552a4aaeb8a4e82359fac41843fa

android: fix AMDGPU asm parser build
With mesa commit 24d4fbe "radeonsi: strengthen emit_optimization_barrier"
AMDGPU asm parser is used and its initialization causes mesa build error:

external/mesa/src/gallium/drivers/radeonsi/si_shader_tgsi_setup.c:130:
error: undefined reference to 'LLVMInitializeAMDGPUAsmParser'

In nougat-x86 branch, and later, AMDGPU asm parser Android.mk
source and tblgen variables need an underscore, to fix a typo,
and tblgen list is updated to avoid the following build errors:

external/llvm/lib/Target/AMDGPU/AsmParser/../MCTargetDesc/AMDGPUMCTargetDesc.h:
fatal error: 'AMDGPUGenRegisterInfo.inc' file not found
fatal error: 'AMDGPUGenInstrInfo.inc' file not found
fatal error: 'AMDGPUGenSubtargetInfo.inc' file not found

Fixes: f223f03 "android: enable support for AMDGPU targets for radeonsi"

lib/Target/AMDGPU/AsmParser/Android.mk
@@ -1,10 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
-amdgpu_asmparser_SRC_FILES := \
+amdgpu_asm_parser_SRC_FILES := \
   AMDGPUAsmParser.cpp
 
-amdgpu_asmparser_TBLGEN_TABLES := \
-  AMDGPUGenAsmMatcher.inc
+amdgpu_asm_parser_TBLGEN_TABLES := \
+  AMDGPUGenAsmMatcher.inc \
+  AMDGPUGenInstrInfo.inc \
+  AMDGPUGenRegisterInfo.inc \
+  AMDGPUGenSubtargetInfo.inc
 
 amdgpu_asm_parser_TBLGEN_TD_DIR := $(LOCAL_PATH)/..
 