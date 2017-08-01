87a2215754f9b15145fdee9c18a8c5a99c8ef098
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 15:37:29 +0800

    INTRINSICTD to INTRINSICTD50
    find . -type f -name "*.mk" -exec sed -i 's/INTRINSICTD/INTRINSICTD50/g' {} +
    
    Change-Id: I042b6a3526260d3d811b4cbba1751e73ffdd0fd6

--- llvm-gen-intrinsics.mk
+++ llvm-gen-intrinsics.mk
@@ -1,6 +1,6 @@
 # We treat Intrinsics.td as a very special target just like what lib/VMCore/Makefile does
-INTRINSICTD := $(LLVM50_ROOT_PATH)/include/llvm/IR/Intrinsics.td
-INTRINSICTDS := $(wildcard $(dir $(INTRINSICTD))/Intrinsics*.td)
+INTRINSICTD50 := $(LLVM50_ROOT_PATH)/include/llvm/IR/Intrinsics.td
+INTRINSICTD50S := $(wildcard $(dir $(INTRINSICTD50))/Intrinsics*.td)
 
 ifeq ($(LOCAL_MODULE_CLASS),)
 	LOCAL_MODULE_CLASS := STATIC_LIBRARIES
@@ -9,7 +9,7 @@ endif
 GENFILE := $(addprefix $(call local-generated-sources-dir)/llvm/IR/,Intrinsics.gen)
 LOCAL_GENERATED_SOURCES += $(GENFILE)
 $(GENFILE): TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
-$(GENFILE): $(INTRINSICTD) $(INTRINSICTDS) | $(LLVM50_TBLGEN)
+$(GENFILE): $(INTRINSICTD50) $(INTRINSICTD50S) | $(LLVM50_TBLGEN)
 ifeq ($(LOCAL_IS_HOST_MODULE),true)
 	$(call transform-host-td-to-out50,intrinsic)
 else
