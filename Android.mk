edbd8074886b9bfae63c7ffe02ddcaf6e3ecf765
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 14:27:55 +0800

    more llvm to llvm70
    Change-Id: I15d009a0b5e2714b9da5acbf224c90777e79a96e

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 LLVM_ROOT_PATH := $(LOCAL_PATH)
+LLVM_TBLGEN := $(HOST_OUT_EXECUTABLES)/llvm70-tblgen
 
 FORCE_BUILD_LLVM_DISABLE_NDEBUG ?= false
 # Legality check: FORCE_BUILD_LLVM_DISABLE_NDEBUG should consist of one word -- either "true" or "false".
