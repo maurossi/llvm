57fc4a1c0efb75dcda7a2e8c1dadeefe5daff350
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 15:01:50 +0800

    LLVM_DEVICE_BUILD_MK to LLVM70_DEVICE_BUILD_MK
    find . -type f -name "*.mk" -exec sed -i 's/LLVM_DEVICE_BUILD_MK/LLVM70_DEVICE_BUILD_MK/g' {} +
    
    Change-Id: If859305d7d6220a8a7cf49a515f09a008162c091

--- lib/Transforms/Vectorize/Android.mk
+++ lib/Transforms/Vectorize/Android.mk
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(transforms_vectorize_SRC_FILES)
 LOCAL_MODULE:= libLLVM70Vectorize
 
-include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM70_DEVICE_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_STATIC_LIBRARY)
