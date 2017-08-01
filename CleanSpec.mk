b1cc21d86517400a8785a88ec37cad831c97e87b
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 14:27:55 +0800

    more llvm to llvm60
    Change-Id: I15d009a0b5e2714b9da5acbf224c90777e79a96e

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -51,7 +51,6 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/product/stingray/obj/STATIC_LIBR
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/product/stingray/obj/STATIC_LIBRARIES/libLLVM60ARMCodeGen_intermediates*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/host/$(HOST_PREBUILT_TAG)/obj/STATIC_LIBRARIES/libLLVM60ARM*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libLLVM60ARM*)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/app_process__asan_intermediates*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libLLVM60*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libLLVM60*)
-$(call add-clean-step, rm -f external/llvm/tools/llvm-config/BuildVariables.inc)
+$(call add-clean-step, rm -f external/llvm60/tools/llvm-config/BuildVariables.inc)
