4f2dbb5d3bf43e5b272df49d1dddb7e2fa33bf7f
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 14:55:37 +0800

    LLVM_ROOT_PATH to LLVM70_ROOT_PATH
    find . -type f -name "*.mk" -exec sed -i 's/LLVM_ROOT_PATH/LLVM70_ROOT_PATH/g' {} +
    
    Change-Id: Ie102b63dbd4200d923a398d57d05f6e56b1e970b

--- tools/llvm-ar/Android.mk
+++ tools/llvm-ar/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-LLVM_ROOT_PATH := $(LOCAL_PATH)/../..
+LLVM70_ROOT_PATH := $(LOCAL_PATH)/../..
 
 
 #===---------------------------------------------------------------===
@@ -28,7 +28,7 @@ LOCAL_LDLIBS += -lpthread -lm -ldl
 LOCAL_POST_INSTALL_CMD = $(hide) ln -sf llvm-ar $(dir $(LOCAL_INSTALLED_MODULE))llvm-lib \
 						 && ln -sf llvm-ar $(dir $(LOCAL_INSTALLED_MODULE))llvm-ranlib
 
-include $(LLVM_ROOT_PATH)/llvm.mk
+include $(LLVM70_ROOT_PATH)/llvm.mk
 include $(LLVM_HOST_BUILD_MK)
 include $(LLVM_GEN_ATTRIBUTES_MK)
 include $(BUILD_HOST_EXECUTABLE)
