2fc85e4890e4c20b14296bf8be1a868b42a8a0dd
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 15:36:24 +0800

    ATTRIBUTETD to ATTRIBUTETD70
    find . -type f -name "*.mk" -exec sed -i 's/ATTRIBUTETD/ATTRIBUTETD70/g' {} +
    
    Change-Id: I03509f4ce804035e3a12a9a36d8158fb2052027d

--- lib/IR/llvm-gen-local.mk
+++ lib/IR/llvm-gen-local.mk
@@ -1,9 +1,9 @@
-ATTRIBUTETD_LOCAL := $(LLVM70_ROOT_PATH)/lib/IR/AttributesCompatFunc.td
+ATTRIBUTETD70_LOCAL := $(LLVM70_ROOT_PATH)/lib/IR/AttributesCompatFunc.td
 
 GENFILE_LOCAL := $(addprefix $(call local-generated-sources-dir)/,AttributesCompatFunc.inc)
 LOCAL_GENERATED_SOURCES += $(GENFILE_LOCAL)
 $(GENFILE_LOCAL): TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
-$(GENFILE_LOCAL): $(ATTRIBUTETD_LOCAL) | $(LLVM70_TBLGEN)
+$(GENFILE_LOCAL): $(ATTRIBUTETD70_LOCAL) | $(LLVM70_TBLGEN)
 ifeq ($(LOCAL_IS_HOST_MODULE),true)
 	$(call transform-host-td-to-out70,attrs)
 else
