5eeabcf64cc402450b68654ca0f8bff29d9e180f
Qiang Yu <Qiang.Yu@amd.com>
2017-08-01 15:36:24 +0800

    ATTRIBUTETD to ATTRIBUTETD50
    find . -type f -name "*.mk" -exec sed -i 's/ATTRIBUTETD/ATTRIBUTETD50/g' {} +
    
    Change-Id: I03509f4ce804035e3a12a9a36d8158fb2052027d

--- lib/IR/llvm-gen-local.mk
+++ lib/IR/llvm-gen-local.mk
@@ -1,9 +1,9 @@
-ATTRIBUTETD_LOCAL := $(LLVM50_ROOT_PATH)/lib/IR/AttributesCompatFunc.td
+ATTRIBUTETD50_LOCAL := $(LLVM50_ROOT_PATH)/lib/IR/AttributesCompatFunc.td
 
 GENFILE_LOCAL := $(addprefix $(call local-generated-sources-dir)/,AttributesCompatFunc.inc)
 LOCAL_GENERATED_SOURCES += $(GENFILE_LOCAL)
 $(GENFILE_LOCAL): TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
-$(GENFILE_LOCAL): $(ATTRIBUTETD_LOCAL) | $(LLVM50_TBLGEN)
+$(GENFILE_LOCAL): $(ATTRIBUTETD50_LOCAL) | $(LLVM50_TBLGEN)
 ifeq ($(LOCAL_IS_HOST_MODULE),true)
 	$(call transform-host-td-to-out50,attrs)
 else
--- llvm-gen-attributes.mk
+++ llvm-gen-attributes.mk
@@ -1,4 +1,4 @@
-ATTRIBUTETD := $(LLVM50_ROOT_PATH)/include/llvm/IR/Attributes.td
+ATTRIBUTETD50 := $(LLVM50_ROOT_PATH)/include/llvm/IR/Attributes.td
 
 ifeq ($(LOCAL_MODULE_CLASS),)
 	LOCAL_MODULE_CLASS := STATIC_LIBRARIES
@@ -7,7 +7,7 @@ endif
 GENFILE := $(addprefix $(call local-generated-sources-dir)/llvm/IR/,Attributes.gen)
 LOCAL_GENERATED_SOURCES += $(GENFILE)
 $(GENFILE): TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
-$(GENFILE): $(ATTRIBUTETD) | $(LLVM50_TBLGEN)
+$(GENFILE): $(ATTRIBUTETD50) | $(LLVM50_TBLGEN)
 ifeq ($(LOCAL_IS_HOST_MODULE),true)
 	$(call transform-host-td-to-out50,attrs)
 else
