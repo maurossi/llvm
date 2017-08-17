1c82bde9d8d0ad51eae2eb5c9298c886aa0bc14d
Mauro Rossi <issor.oruam@gmail.com>
2017-08-17 10:30:50 +0200

    android: fix libLLVM shared build and headers exports (llvm39)
    Similar to android-x86 patch exporting the generated headers in libLLVMCore,
    now applied to libLLVM shared in order to be able to drop libLLVMCore
    static linking in mesa and keep only libLLVM shared library linking.

--- shared_llvm.mk
+++ shared_llvm.mk
@@ -160,7 +160,16 @@ LOCAL_WHOLE_STATIC_LIBRARIES += \
   $(llvm_device_static_libraries) \
   $(llvm_post_static_libraries)
 
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+       $(LOCAL_PATH)/include \
+       $(LOCAL_PATH)/device/include \
+       $(call local-generated-sources-dir)
+
 include $(LLVM39_DEVICE_BUILD_MK)
+include $(LLVM39_GEN_ATTRIBUTES_MK)
+include $(LLVM39_GEN_INTRINSICS_MK)
 include $(BUILD_SHARED_LIBRARY)
 
 endif
