53d07c1eb993204946b70cc0341c48ea33520bc7
Mauro Rossi <issor.oruam@gmail.com>
2018-01-05 00:18:41 +0100

    Revert "Recommit "[Support] Add RetryAfterSignal helper function""
    This reverts commit d04333d38b8fd9914389c21267fa3859b9ebe0b7.

--- include/llvm/Support/Errno.h
+++ include/llvm/Support/Errno.h
@@ -16,7 +16,6 @@
 
 #include <cerrno>
 #include <string>
-#include <type_traits>
 
 namespace llvm {
 namespace sys {
@@ -29,16 +28,6 @@ std::string StrError();
 
 /// Like the no-argument version above, but uses \p errnum instead of errno.
 std::string StrError(int errnum);
-
-template <typename FailT, typename Fun, typename... Args>
-inline auto RetryAfterSignal(const FailT &Fail, const Fun &F,
-                             const Args &... As) -> decltype(F(As...)) {
-  decltype(F(As...)) Res;
-  do
-    Res = F(As...);
-  while (Res == Fail && errno == EINTR);
-  return Res;
-}
 
 }  // namespace sys
 }  // namespace llvm
