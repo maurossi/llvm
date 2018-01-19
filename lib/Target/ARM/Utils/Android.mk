01bbba30921292537f76ba944aa75111bfe8b121
Mauro Rossi <issor.oruam@gmail.com>
2018-01-19 14:10:18 +0100

    android: fix Target/ARM build
    Changelog:
    
    updated list of *.cpp sources in ARM Android.mk makefiles
    added lib/Target/ARM/Utils/Android.mk to build libLLVMARMUtils module
    shared_llvm.mk: added libLLVMARMUtils to arm static libraries
    main Android.mk: lib/Target/ARM/Utils path added
    llvm-tblgen-rules.mk: new rule for ARMGenSystemRegister.inc aligned to AMR Cmakelist.txt
    ARM modules Android.mk: added ARMGenSystemRegister.inc generated file where necessary
    ARM modules Android.mk: added *GEN_ATTRIBUTES_MK, *GEN_INTRINSICS_MK rules where necessary

--- lib/Target/ARM/Utils/Android.mk
+++ lib/Target/ARM/Utils/Android.mk
@@ -1,54 +1,51 @@
 LOCAL_PATH := $(call my-dir)
 
-arm_disassembler_TBLGEN_TABLES70 := \
-  ARMGenDisassemblerTables.inc \
+arm_utils_TBLGEN_TABLES70 := \
+  ARMGenRegisterInfo.inc \
   ARMGenInstrInfo.inc \
   ARMGenSubtargetInfo.inc \
-  ARMGenRegisterInfo.inc \
+  ARMGenSystemOperands.inc \
   ARMGenSystemRegister.inc
 
-arm_disassembler_SRC_FILES := \
-  ARMDisassembler.cpp
+arm_utils_SRC_FILES := \
+  ARMBaseInfo.cpp
 
 # For the device
 # =====================================================
 ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
 include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS70)
 
-TBLGEN_TABLES70 := $(arm_disassembler_TBLGEN_TABLES70)
-
-TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := $(arm_disassembler_SRC_FILES)
+LOCAL_SRC_FILES := $(arm_utils_SRC_FILES)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
-LOCAL_MODULE:= libLLVM70ARMDisassembler
+LOCAL_MODULE:= libLLVM70ARMUtils
+
+TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
+TBLGEN_TABLES70 := $(arm_utils_TBLGEN_TABLES70)
 
 include $(LLVM70_DEVICE_BUILD_MK)
 include $(LLVM70_TBLGEN_RULES_MK)
+include $(LLVM70_GEN_INTRINSICS_MK)
 include $(BUILD_STATIC_LIBRARY)
 endif
 
 # For the host
 # =====================================================
 include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS70)
 
-TBLGEN_TABLES70 := $(arm_disassembler_TBLGEN_TABLES70)
-
-TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := $(arm_disassembler_SRC_FILES)
+LOCAL_SRC_FILES := $(arm_utils_SRC_FILES)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
-LOCAL_MODULE:= libLLVM70ARMDisassembler
+LOCAL_MODULE:= libLLVM70ARMUtils
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
+TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
+TBLGEN_TABLES70 := $(arm_utils_TBLGEN_TABLES70)
+
 include $(LLVM70_HOST_BUILD_MK)
 include $(LLVM70_TBLGEN_RULES_MK)
+include $(LLVM70_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
-
